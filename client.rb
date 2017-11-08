1da40a1a987c64fec51f57238aebcf3806858708
Manuel Ortega <manuel@lasindias.coop>
2017-11-08 12:50:04 +0100

    Renamed cliente.rb to client.rb

---
+++ client.rb
@@ -0,0 +1,22 @@
+require_relative 'lib/activitypub'
+require 'faraday'
+
+class Client
+  def initialize(id)
+    @user_id = id
+  end
+
+  def publish(status, recipients)
+    status = ActivityStreams::Object::Note.new(
+      content: status,
+      to: recipients
+    )
+
+    outbox = ActivityPub::Helper.get_actor(@user_id).outbox
+
+    Faraday.post(
+      "#{outbox}",
+      status.to_json
+    ).body
+  end
+end
