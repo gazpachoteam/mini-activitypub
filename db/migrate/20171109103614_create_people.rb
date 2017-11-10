22387b87781f19d2488644c389e9e0fa71b4e812
Manuel Ortega <manuel@lasindias.coop>
2017-11-10 17:45:06 +0100

    Update People migration

--- db/migrate/20171109103614_create_people.rb
+++ db/migrate/20171109103614_create_people.rb
@@ -2,6 +2,7 @@ class CreatePeople < ActiveRecord::Migration[5.1]
   def change
     create_table :people do |t|
         t.string :username
+        t.string :domain
         t.timestamps
     end
   end
